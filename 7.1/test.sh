f54b2f8448159f2c48a967c01b95d3c20edd0f99
Chingis Sandanov <chingis@wodby.com>
2017-02-15 19:23:55 +0700

    Global revamp

--- 7.1/test.sh
+++ 7.1/test.sh
@@ -2,31 +2,4 @@
 
 set -ex
 
-function checkPhpFpm {
-    make start
-    make stop rm
-}
-
-function checkTools {
-    make run -e CMD='pwd' | grep '/var/www/html'
-    make run -e CMD='composer --version' ENV="-e COMPOSER_ALLOW_SUPERUSER=1"
-    make run -e CMD='ssh -V'
-}
-
-function checkSshKeys {
-    mkdir -p ./test/ssh.tmp
-    ssh-keygen -t dsa -N '' -f ./test/ssh.tmp/id_rsa
-    chmod 700 ./test/ssh.tmp
-    make run -e CMD='[ -f /home/www-data/.ssh/id_rsa ]' VOLUMES="-v ${PWD}/test/ssh.tmp:/mnt/ssh"
-    make run -e CMD='[ -f /home/www-data/.ssh/id_rsa.pub ]' VOLUMES="-v ${PWD}/test/ssh.tmp:/mnt/ssh"
-}
-
-function cleanup {
-    rm -rf ./test/*.tmp
-}
-
-cleanup
-checkPhpFpm
-checkTools
-checkSshKeys
-cleanup
+make run -e CMD='wp cli version' | grep 'WP-CLI'
