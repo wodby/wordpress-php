ffa87a946254ec1dd51c1421dcadf753bd741727
Chingis Sandanov <chingis@wodby.com>
2017-05-19 17:22:35 +0700

    Set check-ready params

--- 7.0/test/4/run.sh
+++ 7.0/test/4/run.sh
@@ -7,7 +7,7 @@ if [[ -n "${DEBUG}" ]]; then
 fi
 
 docker-compose up -d
-docker-compose exec mariadb make check-ready -f /usr/local/bin/actions.mk
-docker-compose exec nginx make check-ready -f /usr/local/bin/actions.mk
+docker-compose exec mariadb make max_try=12 wait_seconds=5 check-ready -f /usr/local/bin/actions.mk
+docker-compose exec nginx make max_try=10 wait_seconds=1 check-ready -f /usr/local/bin/actions.mk
 docker-compose exec --user=82 php tests.sh
 docker-compose down
